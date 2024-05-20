15a107138e2062fa646a204aafddb89b4079a4e0
Arpit <arpitkumar3203@gmail.com>
2024-05-20 23:25:37 +0530

    added piece module

---
+++ src/piece.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from enum import Enum
+
+class PlayingPiece:
+    def __init__(self, piece_type: PieceType):
+        self._piecetype = piece_type
+
+class PlayingPieceX(PlayingPiece):
+    def __init__(self, piece_type: PieceType):
+        super().__init__(piece_type=PieceType.X)
+
+class PlayingPieceo(PlayingPiece):
+    def __init__(self, piece_type: PieceType):
+        super().__init__(piece_type=PieceType.O)
+
+class PlayingPieceAtTheRate(PlayingPiece):
+    def __init__(self, piece_type: PieceType):
+        super().__init__(piece_type=PieceType.ATTHERATE)
+
+class PlayingPieceHash(PlayingPiece):
+    def __init__(self, piece_type: PieceType):
+        super().__init__(piece_type=PieceType.HASH)
+
+class PieceType(Enum):
+    X = 'X'
+    O = 'O'
+    ATTHERATE = '@'
+    HASH = '#'
